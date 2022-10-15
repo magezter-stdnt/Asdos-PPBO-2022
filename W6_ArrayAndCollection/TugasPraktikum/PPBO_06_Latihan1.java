06a655eb958edab039cc19aa595e1f914fde162d
Muhammad Alwiza Ansyar <82679452+alwizzz@users.noreply.github.com>
2022-10-15 12:55:00 +0700

    Update PPBO_06_Latihan1.java

--- W6_ArrayAndCollection/TugasPraktikum/PPBO_06_Latihan1.java
+++ W6_ArrayAndCollection/TugasPraktikum/PPBO_06_Latihan1.java
@@ -1,22 +1,54 @@
 // Nama :
 // NIM  : 
 
+
 public class PPBO_06_Latihan1 {
-public static void main(String[] args) {
-    Kelas[] kelas = new Kelas[10];
+    static class Mahasiswa{
+        // lengkapi kode ini
+    }
     
-    for (int i = 0; i < kelas.length; i++){
-        kelas[i] = new Kelas();
-
-        System.out.println("Kelas " + kelas[i].namaKelas);
+    static class Kelas{
+        // lengkapi kode ini
+    }
+    
+    public static void main(String[] args) {
+        Kelas[] kelas = new Kelas[5];
         
-        for (int j = 0; j < kelas[i].mahasiswa.length; j++){
-            System.out.print(" " + kelas[i].mahasiswa[j].nim);
+        for (int i = 0; i < kelas.length; i++){
+            kelas[i] = new Kelas();
+    
+            System.out.println("Kelas " + kelas[i].namaKelas);
             
+            for (int j = 0; j < kelas[i].mahasiswaArr.length; j++){
+                System.out.print(" " + kelas[i].mahasiswaArr[j].nim);
+                
+            }
+    
+            System.out.println();
         }
-
-        System.out.println();
-        // persons[i].printPerson(;
     }
 }
-}
+
+/*
+Pada soal ini, kalian diminta untuk melengkapi baris kode di dalam class Mahasiswa dan Kelas.
+Kalian tidak perlu mengubah baris kode yang sudah ada. (fungsi main tidak perlu diubah)
+Silakan isi kode sedemikian rupa sehingga program bisa dijalankan dan menghasilkan output
+(ikuti penamaan member class sesuai dengan yg digunakan pada fungsi main)
+
+Output yang diharapkan :
+--------------------------------------------------------------------
+
+Kelas A
+ 00000 00001 00002 00003 00004
+Kelas B
+ 00005 00006 00007 00008 00009
+Kelas C
+ 00010 00011 00012 00013 00014
+Kelas D
+ 00015 00016 00017 00018 00019
+Kelas E
+ 00020 00021 00022 00023 00024
+
+ --------------------------------------------------------------------
+
+ */
